536c0b74d80e35a6ed9c485978b6fb49cbf2d344
Esttiwar <jestiwar.correa@gmail.com>
2022-02-07 20:01:31 -0500

    ajustando medidas para imagen de segunda pantalla

--- src/Private/Film.jsx
+++ src/Private/Film.jsx
@@ -29,22 +29,22 @@ const Film = () => {
   }, [])
 
   return (
-    <div className='bg-container'>
-      <div className='display flex flex-col items-center w-screen'>
-        <h1 className='my-10 font-black text-2xl'>{dataFilm.title}</h1>
-        <img className='mb-10' src={dataFilm.image} alt="dataFilm img" />
-        <div className='text-center px-10'>
-          <p>Description: {dataFilm.description}</p>
-          <br />
-          <p><p className='font-black'>Director: </p>{dataFilm.director}</p>
-          <p><p className='font-black'>Producer: </p>{dataFilm.producer}</p>
-          <p><p className='font-black'>Original title: </p>{dataFilm.original_title}</p>
-          <p><p className='font-black'>Original title romanised: </p>{dataFilm.original_title_romanised}</p>
-          <p><p className='font-black'>Rotten Tomatoes score: </p>{dataFilm.rt_score}</p>
-          <p><p className='font-black'>Release date: </p>{dataFilm.release_date}</p>
-          <p><p className='font-black'>Running time: </p>{dataFilm.running_time} minutes</p>
+    <div className='display flex justify-center film bg-container px-20 h-screen w-full'>
+        <div className='display w-1/2 flex flex-col items-center border-solid border-2 border-red-600'>
+          <h1 className='my-10 font-black text-2xl'>{dataFilm.title}</h1>
+          <img className='mb-10 h-3/4' src={dataFilm.image} alt="dataFilm img" />
         </div>
-      </div>
+        <div className='text-start pt-28 px-10 w-1/2'>
+          <p><p className='font-black'>Description: </p>{dataFilm.description}</p>
+          <br />
+          <p className='display flex'><p className='font-black'>Director: &nbsp; </p>{dataFilm.director}</p>
+          <p className='display flex'><p className='font-black'>Producer: &nbsp;</p>{dataFilm.producer}</p>
+          <p className='display flex'><p className='font-black'>Original title: &nbsp;</p>{dataFilm.original_title}</p>
+          <p className='display flex'><p className='font-black'>Original title romanised: &nbsp;</p>{dataFilm.original_title_romanised}</p>
+          <p className='display flex'><p className='font-black'>Rotten Tomatoes score: &nbsp;</p>{dataFilm.rt_score}</p>
+          <p className='display flex'><p className='font-black'>Release date: &nbsp;</p>{dataFilm.release_date}</p>
+          <p className='display flex'><p className='font-black'>Running time: &nbsp;</p>{dataFilm.running_time} minutes</p>
+        </div>
     </div>
   );
 };
--- src/Public/Unauth.jsx
+++ src/Public/Unauth.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Login from './Login';
 
 const Unauth = () => {
   return (
     <Routes>
         <Route path="/" element={<Login />} />
+        <Route path='*' element={<Navigate to="/" />} />
     </Routes>
   );
 };
