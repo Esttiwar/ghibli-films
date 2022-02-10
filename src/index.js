892ebcf8fbcb776346e866287195b0d6514810ec
Esttiwar <jestiwar.correa@gmail.com>
2022-02-09 21:36:23 -0500

    agregando hashrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { AuthProvider } from "./Context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter> 
+      </HashRouter> 
     </AuthProvider>  
   </React.StrictMode>,
   document.getElementById('root')
